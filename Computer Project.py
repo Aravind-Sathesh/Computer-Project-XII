bf791d549152719a767f7a7c9d73adcb60e1518f
Aravind-Sathesh <98135310+Aravind-Sathesh@users.noreply.github.com>
2022-06-02 21:57:54 +0530

    Added comments

--- Computer Project.py
+++ Computer Project.py
@@ -1,18 +1,18 @@
 # Computer Science Project
 # Movie Theatre
 
-from ctypes import alignment
-from pydoc import text
 from tkinter import *
 import tkinter
 from tkinter.messagebox import askyesno
 
 # with open('Films.txt','a+') as file :
 #     file.write('Movie List')
+
+# Setting values for some common colours:
 foreg = 'black'
 backg = 'white'
 
-
+# Creating output window and setting attributes:
 window = Tk()
 window.title('Home Screen')
 window.attributes('-fullscreen', True)
@@ -21,15 +21,15 @@ film_var = StringVar()
 rating = DoubleVar()
 
 
-def closewindow():
+def closewindow():  # Function to close button when exit button is clicked
     window.destroy()
 
 
-def review():
+def review():  # Function to receive user rating
     pass
 
 
-def thankyou():
+def thankyou():  # Function to be displayed when booking is done
     askyesno(title='Thankyou', message='Hope you had a wonderful time')
 
 
@@ -37,7 +37,7 @@ def ask_number_people():
     window1 = Toplevel(window)
     window1.geometry('250x200')
     window1.resizable(False, False)
-    label_ask = Button(window1, text='Please enter the number of people', alignment=CENTER,
+    label_ask = Button(window1, text='Please enter the number of people',
                        width=26, height=3, font=('Arial', 10))
     label_ask.place(x=15, y=0)
     askbox = Entry(window1, background='white',
@@ -45,7 +45,8 @@ def ask_number_people():
     askbox.place(x=0, y=100)
 
 
-welcome_label = Label(window, fg='#336EFF', activeforeground='#336EFF',
+# Displaying labels and setting attributes
+welcome_label = Label(window, fg='#336EFF',
                       width=30, height=2, text='Welcome to NCFE Cinemas', font=('Algerian', 30))
 name_label = Label(window, fg=foreg, bg=backg, activeforeground=foreg, activebackground=backg,
                    width=20, height=1, text='Please enter your name', font=('Arial', 25))
@@ -56,24 +57,24 @@ film_label = Label(window, fg=foreg, bg=backg, activeforeground=foreg, activebac
 input_box = Entry(window, background='white',
                   foreground='black', width=21, font=('Arial', 25))
 
-
-number_people_button_1 = Radiobutton(window, activebackground='#FFFFFF', activeforeground='#1E66DA', font=(
+# Displaying buttons and setting attributes
+number_people_button_1 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
     'ComicSans', '25'), text='1', variable=people_var, value=1)
-number_people_button_2 = Radiobutton(window, activebackground='#FFFFFF', activeforeground='#1E66DA', font=(
+number_people_button_2 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
     'ComicSans', '25'), text='2', variable=people_var, value=2)
-number_people_button_3 = Radiobutton(window, activebackground='#FFFFFF', activeforeground='#1E66DA', font=(
+number_people_button_3 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
     'ComicSans', '25'), text='3', variable=people_var, value=3)
-number_people_button_4 = Radiobutton(window, activebackground='#FFFFFF', activeforeground='#1E66DA', font=(
+number_people_button_4 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
     'ComicSans', '25'), text='4', variable=people_var, value=4)
-number_people_button_5 = Radiobutton(window, activebackground='#FFFFFF', activeforeground='#1E66DA', font=(
+number_people_button_5 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
     'ComicSans', '25'), text='5', variable=people_var, value=5)
-number_people_button_5plus = Radiobutton(window, activebackground='#FFFFFF', activeforeground='#1E66DA', font=(
+number_people_button_5plus = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
     'ComicSans', '25'), text='5+', variable=people_var, value=6)
-film_button1 = Radiobutton(window, activebackground='#FFFFFF', activeforeground='#1E66DA', font=(
+film_button1 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
     'ComicSans', '25'), text='Film1', variable=film_var, value='Film1')
-film_button2 = Radiobutton(window, activebackground='#FFFFFF', activeforeground='#1E66DA', font=(
+film_button2 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
     'ComicSans', '25'), text='Film2', variable=film_var, value='Film2')
-film_button3 = Radiobutton(window, activebackground='#FFFFFF', activeforeground='#1E66DA', font=(
+film_button3 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
     'ComicSans', '25'), text='Film3', variable=film_var, value='Film3')
 rating_bar = Scale(window, variable=rating, orient=HORIZONTAL, bd=3,
                    font=('Arial', 20), from_=0, to=10, length=400)
@@ -84,6 +85,7 @@ exit_button = Button(window, text='Exit', width=10, command=closewindow,
 register_button = Button(
     window, text='Proceed to booking', font=('Comic Sans', 25))
 
+# Setting location of buttons, labels, etc. in x and y coordinates:
 name_label.place(x=30, y=150)
 welcome_label.place(x=400, y=0)
 number_people_label.place(x=522, y=150)
@@ -103,5 +105,5 @@ rate_button.place(x=910, y=797)
 exit_button.place(x=1410, y=795)
 register_button.place(x=60, y=300)
 
-
+# Required command to launch output window
 window.mainloop()
