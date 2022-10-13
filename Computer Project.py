cf7195c82ba0c7bc58f71b58c5a987e5bef97ac0
Aravind Sathesh <aravind.sathesh@ncfe.ac.in>
2022-10-13 15:28:06 +0530

    Increased speed and decreeased number of lines

--- Computer Project.py
+++ Computer Project.py
@@ -2,34 +2,25 @@
 # Movie Theatre
 
 from tkinter import *
-import csv
 from tkinter.messagebox import askyesno
 
-# with open('Films.txt','a+') as file :
-#     file.write('Movie List')
-
-# Setting values for some universal values:
-window_size = '1500x800'
 foreg = 'black'
 backg = 'white'
 
 # Creating output window and setting attributes:
 window = Tk()
 window.title('Home Screen')
-window.geometry(window_size)
+window.geometry('1500x800')
 window.resizable(False, False)
 people_var = IntVar()
 film_var = StringVar()
 rating = DoubleVar()
 
-
 def closewindow():  # Function to close button when exit button is clicked
     window.destroy()
 
-
 def thankyou():  # Function to be displayed when booking is done
     askyesno(title='Thankyou', message='Did you enjoy the booking experience?')
-
 
 def add_person():  # Adds a dictionary to a file
     with open('Films.csv', 'a+') as file:
@@ -39,8 +30,7 @@ def add_person():  # Adds a dictionary to a file
         dict1['Persons'] = people_var.get()
         file.write(f'{dict1}\n')
 
-
-def ask_number_people():
+def ask_number_people():  # Function to take input of number of people the ticket is being booked for
     window1 = Toplevel(window)
     window1.geometry('250x200')
     window1.resizable(False, False)
@@ -52,72 +42,45 @@ def ask_number_people():
     askbox.place(x=0, y=100)
 
 
+# Standard functions for button and label:
+def RADIOBUTTON(t, xloc, yloc, activebg = 'white', activefg = '#1E66DA', f=('ComicSans', '25'), var=film_var):
+    button = Radiobutton(window, activebackground = activebg, activeforeground=activefg, font=f, text=t, variable=var, value=t)
+    button.place(x=xloc, y=yloc)
+def LABEL(t, xloc, yloc, foreg = 'black', backg='white', w=20, h=1, f=('Helvetica', 25)):
+    label = Label(window, fg=foreg, bg=backg, activeforeground=foreg, activebackground=backg, width=w, height=h, text=t, font=f)
+    label.place(x=xloc, y=yloc)
+
 # Displaying labels and setting attributes
-welcome_label = Label(window, fg='#336EFF',
-                      width=30, height=2, text='Welcome to NCFE Miniplex', font=('Algerian', 30))
-name_label = Label(window, fg=foreg, bg=backg, activeforeground=foreg, activebackground=backg,
-                   width=20, height=1, text='Please enter your name', font=('Helvetica', 25))
-number_people_label = Label(window, fg=foreg, bg=backg, activeforeground=foreg, activebackground=backg,
-                            width=27, height=1, text='Please choose number of viewers', font=('Helvetica', 25))
-film_label = Label(window, fg=foreg, bg=backg, activeforeground=foreg, activebackground=backg,
-                   width=20, height=1, text='Please choose movie', font=('Helvetica', 25))
-input_box = Entry(window, background='white',
-                  foreground='black', width=21, font=('Helvetica', 25))
+LABEL('Welcome to NCFE Miniplex', 400, 0, foreg='#336EFF')
+LABEL('Please enter your name', 580, 125)
+LABEL('Please choose number of viewers', 515, 275, w = 27)
+LABEL('Please choose movie', 585, 460)
 
 # Displaying buttons and setting attributes
-people_bar = Scale(window, variable=rating, orient=HORIZONTAL, bd=1,
-                   font=('Helvetica', 20), from_=1, to=10, length=400, command=people_var)
-film_button1 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
-    'ComicSans', '25'), text='Film1', variable=film_var, value='Film1')
-film_button2 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
-    'ComicSans', '25'), text='Film2', variable=film_var, value='Film2')
-film_button3 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
-    'ComicSans', '25'), text='Film3', variable=film_var, value='Film3')
-film_button4 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
-    'ComicSans', '25'), text='Film4', variable=film_var, value='Film4')
-film_button5 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
-    'ComicSans', '25'), text='Film5', variable=film_var, value='Film5')
-film_button6 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
-    'ComicSans', '25'), text='Film6', variable=film_var, value='Film6')
-film_button7 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
-    'ComicSans', '25'), text='Film7', variable=film_var, value='Film7')
-film_button8 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
-    'ComicSans', '25'), text='Film8', variable=film_var, value='Film8')
-film_button9 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
-    'ComicSans', '25'), text='Film9', variable=film_var, value='Film9')
-film_button10 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
-    'ComicSans', '25'), text='Film10', variable=film_var, value='Film10')
-film_button11 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
-    'ComicSans', '25'), text='Film11', variable=film_var, value='Film11')
-film_button12 = Radiobutton(window, activebackground='white', activeforeground='#1E66DA', font=(
-    'ComicSans', '25'), text='Film12', variable=film_var, value='Film12')
-exit_button = Button(window, text='Exit', width=10, command=closewindow,
-                     height=1, font=('Helvetica', 15))
-submit_button = Button(window, width=10,
-                       text='Submit', font=('Helvetica', 16))
+RADIOBUTTON('Film 1', 550, 525)
+RADIOBUTTON('Film 2', 550, 575)
+RADIOBUTTON('Film 3', 550, 625)
+RADIOBUTTON('Film 4', 550, 675)
+RADIOBUTTON('Film 5', 700, 525)
+RADIOBUTTON('Film 6', 700, 575)
+RADIOBUTTON('Film 7', 700, 625)
+RADIOBUTTON('Film 8', 700, 675)
+RADIOBUTTON('Film 9', 850, 525)
+RADIOBUTTON('Film 10', 850, 575)
+RADIOBUTTON('Film 11', 850, 625)
+RADIOBUTTON('Film 12', 850, 675)
 
-# Setting location of buttons, labels, etc. in x and y coordinates:
-welcome_label.place(x=400, y=0)
-
-name_label.place(x=580, y=125)
+# Displaying other buttons and setting respective attributes
+input_box = Entry(window, background='white',foreground='black', width=21, font=('Helvetica', 25))
 input_box.place(x=582, y=185)
 
-number_people_label.place(x=515, y=275)
+people_bar = Scale(window, variable=rating, orient=HORIZONTAL, bd=1, font=('Helvetica', 20), from_=1, to=10, length=400, command=people_var)
 people_bar.place(x=572, y=325)
-film_label.place(x=585, y=460)
-film_button1.place(x=550, y=525)
-film_button2.place(x=550, y=575)
-film_button3.place(x=550, y=625)
-film_button4.place(x=550, y=675)
-film_button5.place(x=700, y=525)
-film_button6.place(x=700, y=575)
-film_button7.place(x=700, y=625)
-film_button8.place(x=700, y=675)
-film_button9.place(x=850, y=525)
-film_button10.place(x=850, y=575)
-film_button11.place(x=850, y=625)
-film_button12.place(x=850, y=675)
+
+exit_button = Button(window, text='Exit', width=10, command=closewindow, height=1, font=('Helvetica', 15))
 exit_button.place(x=1375, y=755)
+
+submit_button = Button(window, width=10, text='Submit', font=('Helvetica', 16), command=screen2)
 submit_button.place(x=710, y=755)
 
 # Required command to launch output window
